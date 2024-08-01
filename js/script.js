ea1cd9db50251a2e6b30cf92cf35a46723a047e3
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-08-01 20:58:39 +0300

    fix scroll

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
+// Функція прокрутки донизу
+function scrollToBottom() {
+    const chatContainer = document.getElementById('message-container');
+    if (chatContainer) {
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+}
 
 const messageAnswer = document.querySelector('div.message-answer');
 const answerYes = document.querySelector('.yes');
 const answerNo = document.querySelector('.no');
 const questionMessageList = document.querySelector('.message-list');
 const formContainer = document.querySelector('.form');
+
 if (formContainer) {
     formContainer.style.display = 'none';
 }
@@ -13,28 +21,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const btns = document.querySelector('.message-answer');
     let delay = 0;
 
-    function scrollToBottom() {
-        questionMessageList.scrollTop = questionMessageList.scrollHeight
-      }
-
     messages.forEach((message, index) => {
         setTimeout(() => {
-            message.classList.add('visible');
-            // message.classList.remove('hidden');
-            scrollToBottom()
-            if (index === messages.length - 1) {
-                // Показуємо блок з кнопками після останнього повідомлення
-                setTimeout(() => {
-                    btns.classList.add('visible');
-                    // btns.classList.remove('hidden');
-                    scrollToBottom()
-                }, 600); // Додаємо затримку в 1 секунду після останнього повідомлення
+            if (message) {
+                message.classList.add('visible');
+                scrollToBottom(); // Після відображення повідомлення
+                if (index === messages.length - 1) {
+                    setTimeout(() => {
+                        if (btns) {
+                            btns.classList.add('visible');
+                            scrollToBottom(); // Після відображення кнопок
+                        }
+                    }, 600); // Затримка
+                }
             }
         }, delay);
         delay += 1000; // Затримка 1 секунда між повідомленнями
     });
 });
-
 
 function addAnswer(answerText) {
     const newAnswer = document.createElement('li');
@@ -43,11 +47,16 @@ function addAnswer(answerText) {
         <p class="answer-item__text">${answerText}</p>
         <img src="./img/teil.svg" alt="decoration img" class="answer-tail"/>
     `;
-    questionMessageList.appendChild(newAnswer);
-    setTimeout(() => {
-        newAnswer.classList.add('visible');
-    }, 100);
-    messageAnswer.style.display = 'none';
+    if (questionMessageList) {
+        questionMessageList.appendChild(newAnswer);
+        setTimeout(() => {
+            newAnswer.classList.add('visible');
+            scrollToBottom(); // Прокрутка після додавання нової відповіді
+        }, 100);
+    }
+    if (messageAnswer) {
+        messageAnswer.style.display = 'none';
+    }
 }
 
 function handleAnswerNoClick() {
@@ -80,110 +89,123 @@ function questionStep(question, answers, isFinalStep = false) {
         <p class="message-time">11:45</p>
         <img src="./img/teil.svg" alt="decoration img" class="message-tail"/>
     `;
-    questionMessageList.appendChild(newQuestion);
-    setTimeout(() => {
-        newQuestion.classList.add('visible');
-    }, 100);
+    if (questionMessageList) {
+        questionMessageList.appendChild(newQuestion);
+        setTimeout(() => {
+            newQuestion.classList.add('visible');
+            scrollToBottom(); // Прокрутка після додавання нового питання
+        }, 100);
 
-    setTimeout(() => {
-        messageAnswer.innerHTML = answers;
-        messageAnswer.style.display = 'flex';
+        setTimeout(() => {
+            if (messageAnswer) {
+                messageAnswer.innerHTML = answers;
+                messageAnswer.style.display = 'flex';
 
-        const ansYes = document.getElementById('answer-yes');
-        const ansNo = document.getElementById('answer-no');
-        const ansHear = document.getElementById('answer-hear');
-        const ansOne = document.getElementById('answer-one');
-        const ansTwo = document.getElementById('answer-two');
-        const ansFive = document.getElementById('answer-five');
+                const ansYes = document.getElementById('answer-yes');
+                const ansNo = document.getElementById('answer-no');
+                const ansHear = document.getElementById('answer-hear');
+                const ansOne = document.getElementById('answer-one');
+                const ansTwo = document.getElementById('answer-two');
+                const ansFive = document.getElementById('answer-five');
 
-        const handleAnswerClick = (answerText) => {
-            addAnswer(answerText);
-            if (isFinalStep) {
-                const finalMessage = document.createElement('li');
-                finalMessage.classList.add('message-item');
-                finalMessage.innerHTML = `
-                    <p class="message-text">Дякую! Наша компанія дуже зацікавлена ​​вами, для подальшої підтримки зв'язку, будь ласка, заповніть форму.</p>
-                    <p class="message-time">11:45</p>
-                    <img src="./img/teil.svg" alt="decoration img" class="message-tail"/>
-                `;
-                questionMessageList.appendChild(finalMessage);
-                formContainer.style.display = 'flex';
-                setTimeout(() => {
-                    finalMessage.classList.add('visible');
-                }, 100);
+                const handleAnswerClick = (answerText) => {
+                    addAnswer(answerText);
+                    if (isFinalStep) {
+                        const finalMessage = document.createElement('li');
+                        finalMessage.classList.add('message-item');
+                        finalMessage.innerHTML = `
+                            <p class="message-text">Дякую! Наша компанія дуже зацікавлена ​​вами, для подальшої підтримки зв'язку, будь ласка, заповніть форму.</p>
+                            <p class="message-time">11:45</p>
+                            <img src="./img/teil.svg" alt="decoration img" class="message-tail"/>
+                        `;
+                        questionMessageList.appendChild(finalMessage);
+                        if (formContainer) {
+                            formContainer.style.display = 'flex';
+                        }
+                        setTimeout(() => {
+                            finalMessage.classList.add('visible');
+                            scrollToBottom(); // Прокрутка після додавання фінального повідомлення
+                        }, 100);
+                    }
+                };
+
+                if (ansYes) {
+                    ansYes.addEventListener('click', () => {
+                        handleAnswerClick('Так');
+                        if (!isFinalStep) {
+                            setTimeout(() => {
+                                questionStep(
+                                    "Скільки часу ви могли б приділяти на день?",
+                                    `<button class="message-answer__btn" id='answer-one'>Одна година</button>
+                                     <button class="message-answer__btn" id='answer-two'>2-3 години</button>
+                                     <button class="message-answer__btn" id='answer-five'>5 і більше</button>`,
+                                    true
+                                );
+                            }, 500);
+                        }
+                    }, { once: true });
+                }
+
+                if (ansNo) {
+                    ansNo.addEventListener('click', () => {
+                        handleAnswerClick('Ні');
+                        if (!isFinalStep) {
+                            setTimeout(() => {
+                                questionStep(
+                                    "Скільки часу ви могли б приділяти на день?",
+                                    `<button class="message-answer__btn" id='answer-one'>Одна година</button>
+                                     <button class="message-answer__btn" id='answer-two'>2-3 години</button>
+                                     <button class="message-answer__btn" id='answer-five'>5 і більше</button>`,
+                                    true
+                                );
+                            }, 500);
+                        }
+                    }, { once: true });
+                }
+
+                if (ansHear) {
+                    ansHear.addEventListener('click', () => {
+                        handleAnswerClick('Чув про це');
+                        if (!isFinalStep) {
+                            setTimeout(() => {
+                                questionStep(
+                                    "Скільки часу ви могли б приділяти на день?",
+                                    `<button class="message-answer__btn" id='answer-one'>Одна година</button>
+                                     <button class="message-answer__btn" id='answer-two'>2-3 години</button>
+                                     <button class="message-answer__btn" id='answer-five'>5 і більше</button>`,
+                                    true
+                                );
+                            }, 500);
+                        }
+                    }, { once: true });
+                }
+
+                if (ansOne) {
+                    ansOne.addEventListener('click', () => {
+                        handleAnswerClick('Одна година');
+                    }, { once: true });
+                }
+
+                if (ansTwo) {
+                    ansTwo.addEventListener('click', () => {
+                        handleAnswerClick('2-3 години');
+                    }, { once: true });
+                }
+
+                if (ansFive) {
+                    ansFive.addEventListener('click', () => {
+                        handleAnswerClick('5 і більше');
+                    }, { once: true });
+                }
             }
-        };
-
-        if (ansYes) {
-            ansYes.addEventListener('click', () => {
-                handleAnswerClick('Так');
-                if (!isFinalStep) {
-                    setTimeout(() => {
-                        questionStep(
-                            "Скільки часу ви могли б приділяти на день?",
-                            `<button class="message-answer__btn" id='answer-one'>Одна година</button>
-                             <button class="message-answer__btn" id='answer-two'>2-3 години</button>
-                             <button class="message-answer__btn" id='answer-five'>5 і більше</button>`,
-                            true
-                        );
-                    }, 500);
-                }
-            }, { once: true });
-        }
-
-        if (ansNo) {
-            ansNo.addEventListener('click', () => {
-                handleAnswerClick('Ні');
-                if (!isFinalStep) {
-                    setTimeout(() => {
-                        questionStep(
-                            "Скільки часу ви могли б приділяти на день?",
-                            `<button class="message-answer__btn" id='answer-one'>Одна година</button>
-                             <button class="message-answer__btn" id='answer-two'>2-3 години</button>
-                             <button class="message-answer__btn" id='answer-five'>5 і більше</button>`,
-                            true
-                        );
-                    }, 500);
-                }
-            }, { once: true });
-        }
-
-        if (ansHear) {
-            ansHear.addEventListener('click', () => {
-                handleAnswerClick('Чув про це');
-                if (!isFinalStep) {
-                    setTimeout(() => {
-                        questionStep(
-                            "Скільки часу ви могли б приділяти на день?",
-                            `<button class="message-answer__btn" id='answer-one'>Одна година</button>
-                             <button class="message-answer__btn" id='answer-two'>2-3 години</button>
-                             <button class="message-answer__btn" id='answer-five'>5 і більше</button>`,
-                            true
-                        );
-                    }, 500);
-                }
-            }, { once: true });
-        }
-
-        if (ansOne) {
-            ansOne.addEventListener('click', () => {
-                handleAnswerClick('Одна година');
-            }, { once: true });
-        }
-
-        if (ansTwo) {
-            ansTwo.addEventListener('click', () => {
-                handleAnswerClick('2-3 години');
-            }, { once: true });
-        }
-
-        if (ansFive) {
-            ansFive.addEventListener('click', () => {
-                handleAnswerClick('5 і більше');
-            }, { once: true });
-        }
-    }, 100);
+        }, 100);
+    }
 }
 
+const observer = new MutationObserver(scrollToBottom);
+const chatContainer = document.getElementById('message-container');
+if (chatContainer) {
+    observer.observe(chatContainer, { childList: true, subtree: true });
+}
 
-
+scrollToBottom();
